14cd09a4fd2d51cd1bf238bc3aaa3ea65f2ffba5
Hilary Bidlake <47711475+automaticdog@users.noreply.github.com>
2023-03-26 16:58:11 -0700

    Build out basic js functionality of todo list app

--- 201-todo_list_project.js
+++ 201-todo_list_project.js
@@ -19,3 +19,48 @@
 //     - Logs toast message "Todo removed"
 //   5. "Quit":
 //     - Exits the "app" (break the top-level prompt loop)
+
+function todoApp() {
+  let isActive = true;
+  const todoItems = [];
+  let validActions = ["new", "list", "delete", "quit"]
+  while (isActive === true) {
+    let currentAction = prompt(`What would you like to do? You can try:
+      "New": Add new todo
+      "List": List current todos
+      "Delete": Delete some todos
+      "Quit": Quit the app`)
+    if (validActions.indexOf(currentAction.toLowerCase()) == -1) {
+      currentAction = prompt("Oops! I don't know how to do that. You can say: 'New', 'List', 'Delete', or 'Quit'")
+    } else {
+      if (currentAction.toLowerCase() == "new") {
+        todoItems.push(prompt("Enter new todo item:"))
+      } else if (currentAction.toLowerCase() == "list") {
+        if (todoItems.length == 0) {
+          console.log("You have no todo items yet!")
+        } else {
+          console.log("*******")
+          for (let item of todoItems) {
+            console.log(`${todoItems.indexOf(item)}: ${item}`)
+          }
+          console.log("*******")
+        }
+      } else if (currentAction.toLowerCase() == "delete") {
+        let deleted = prompt(`Enter index of item to delete, or enter "clear list" to delete all items:`);
+
+        if (deleted.toLowerCase == "clear list") {
+          for (let item in todoItems) {
+            todoItems.pop(item);
+          }
+        } else if (todoItems.indexOf(deleted) == -1) {
+          console.log("Looks like that item doesn't exist :/");
+        } else {
+          todoItems.pop(parseInt(todoItems[deleted]));
+        }
+      } else if (currentAction.toLowerCase() == "quit") {
+        console.log("Ok, quitting the app!")
+        isActive = false;
+      }
+    }
+  }
+}
