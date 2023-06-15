419972e8ea5dc0feceb29f853b796cfa0c4b0164
Hilary Bidlake <47711475+automaticdog@users.noreply.github.com>
2023-06-15 14:15:04 +0100

    Remove unused variables/functions and fix missing logic caused by merge

--- ScoreKeeper/app.js
+++ ScoreKeeper/app.js
@@ -1,55 +1,18 @@
 const input = document.querySelector('#select');
-const scoreDisplay = document.querySelector('#score')
-const buttons = document.querySelector('.btn-row');
-
+const resetBtn = document.querySelector('#reset');
 const addPlayerOne = document.querySelector('#player-one');
 const addPlayerTwo = document.querySelector('#player-two');
-
-const resetBtn = document.querySelector('#reset');
-
-let maxRounds = 1;
-let currentScore = '';
-let playerOneScore = 0;
-let playerTwoScore = 0;
 
 const playerOneDisplay = document.querySelector('#player1');
 const playerTwoDisplay = document.querySelector('#player2');
 
-function updateScores() {
-  playerOneDisplay.innerText = playerOneScore;
-  playerTwoDisplay.innerText = playerTwoScore;
-}
+let maxRounds = input.value;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
-// WHEN A PLAYER BUTTON IS CLICKED:
-// 1) updated that player's displayed score value
-// 2) check if either score == maxScore
-
-addPlayerOne.addEventListener("click", () => {
-  // console.log("player 1 clicked!")
-  playerOneScore += 1;
-  updateScores();
-  checkScore();
-})
-
-addPlayerTwo.addEventListener("click", () => {
-  // console.log("player 2 clicked!")
-  playerTwoScore += 1;
-  updateScores();
-  checkScore();
-})
-
-function enableButtons() {
-  console.log("enabling buttons!")
-
-  addPlayerOne.classList.remove("disabled");
-  addPlayerTwo.classList.remove("disabled");
-}
-
-updateScores();
 
 // set maxRounds value when user selects a score to play to
 input.addEventListener("change", () => {
-  console.log('input selected')
   maxRounds = input.value;
 })
 
@@ -57,36 +20,52 @@ input.addEventListener("change", () => {
 // 1) updated that player's displayed score value
 // 2) check if either score == maxScore
 
+function updateScores() {
+  playerOneDisplay.innerText = playerOneScore;
+  playerTwoDisplay.innerText = playerTwoScore;
+}
+
 addPlayerOne.addEventListener("click", () => {
   // console.log("player 1 clicked!")
   playerOneScore += 1;
   updateScores();
-  checkScore();
+  if (playerOneScore >= maxRounds) {
+    setWinner(playerOneDisplay);
+    setLoser(playerTwoDisplay);
+    disableButtons();
+  }
 })
 
 addPlayerTwo.addEventListener("click", () => {
   // console.log("player 2 clicked!")
   playerTwoScore += 1;
   updateScores();
-  checkScore();
+  if (playerTwoScore >= maxRounds) {
+    setWinner(playerTwoDisplay);
+    setLoser(playerOneDisplay);
+    disableButtons();
+  }
 })
 
-function compareScores() {
-  console.log('comparing scores');
-  if (playerOneScore > playerTwoScore) {
-    console.log("first condition")
-    playerOneDisplay.classList.add('winner');
-    playerTwoDisplay.classList.add('loser');
-    console.log(playerOneDisplay.classList)
-  } else {
-    setLoser(playerOneDisplay);
-    setWinner(playerTwoDisplay);
-  }
+function setWinner(player) {
+  player.classList.add('winner');
+}
+function setLoser(player) {
+  player.classList.add('loser');
+}
+
+function disableButtons() {
+  console.log("disabling buttons!")
+  addPlayerOne.classList.toggle("disabled");
+  addPlayerTwo.classList.toggle("disabled");
+}
+function enableButtons() {
+  addPlayerOne.classList.remove("disabled");
+  addPlayerTwo.classList.remove("disabled");
 }
 
 // RESET THE GAME:
 resetBtn.addEventListener("click", function() {
-  console.log("reset clicked!")
   resetGame();
 })
 
@@ -94,11 +73,10 @@ resetBtn.addEventListener("click", function() {
 // 2) reset maxScore input to 1
 
 function resetGame() {
-  console.log("resetting game!")
-  maxRounds = input.value;
   playerOneScore = 0;
   playerTwoScore = 0;
   playerOneDisplay.classList = '';
   playerTwoDisplay.classList = '';
   updateScores();
+  enableButtons();
 }
